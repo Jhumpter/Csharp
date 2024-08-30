951ab912d1fde1e4c45bd97c68e3dda451e67612
Jhumpter <joaopwii@gmail.com>
2024-08-30 19:44:37 -0300

    Escrevendo no console

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+string mensagemBoasVindas = "Bem vindo ao Screen Sound";
+// No C# as aspas duplas ("") são mais usuais e o ; é obrigatório
+Console.WriteLine(mensagemBoasVindas); // Escreve no console
