cabb4e6f02d441f7bcc9b05070b84717b75df220
Jhumpter <joaopwii@gmail.com>
2024-08-30 20:09:13 -0300

    Criando menu

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,10 +1,32 @@
 ﻿// Screen Sound
 string mensagemBoasVindas = "Bem vindo ao Screen Sound";
 // No C# as aspas duplas ("") são mais usuais e o ; é obrigatório
+// Console.WriteLine() para escrever no console
 // Para criar uma função sem return utiliza-se void
 void ExibirMensagem()
-{
-    Console.WriteLine("*************************");
+{   // Para criar um verbatim literal, utiliza-se @ antes das aspas
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
     Console.WriteLine(mensagemBoasVindas);
-    Console.WriteLine("*************************");
 }
+
+void ExibirMenu()
+{
+    Console.WriteLine(@"
+Digite 1 para adicionar uma banda
+Digite 2 para ver todas as bandas
+Digite 3 para avaliar uma banda
+Digite 4 para exibir a média de uma banda
+Digite -1 para sair
+");
+}
+
+ExibirMensagem();
+ExibirMenu();
